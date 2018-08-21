11b259203f14bf55c9d8458edbdecc4520113792
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-08-21 11:41:58 +0200

    routes and cont

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -1,23 +1,47 @@
 class TalentsController < ApplicationController
-  def new
-  end
+  before_action :find_talent, only: [:show, :edit, :update, :destroy]
 
-  def create
-  end
 
-  def update
+  def show
   end
 
   def edit
   end
 
+  def create
+    @talent = Talent.new(talent_params)
+    if @talent.save
+      redirect_to talent_path(@talent)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @talent.update(talent_params)
+    redirect_to talent_path
+  end
+
   def destroy
+    @talent.destroy
+    redirect_to talents_path
+  end
+
+  def new
+    @talent = Talent.new
   end
 
   def index
+    @talents = Talent.all
   end
 
-  def show
+  private
+
+  def find_talent
+    @talent = Talent.find(params[:id])
   end
-end
+
+  def talent_params
+    params.require(:talent).permit(:title, :description, :picture)
+  end
 end
