e2b7be88b6e1be78bcbb4a748811b89fdd75053a
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-08-21 17:12:58 +0200

    added foreign keys to Talent

--- app/models/talent.rb
+++ app/models/talent.rb
@@ -2,6 +2,9 @@ class Talent < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :bookings
+  has_many :booked_talents, through: :bookings, class_name: "User"
   has_many :reviews
+  has_many :reviewed_talents, through: :reviews, source: :user
   belongs_to :user
 end
+
