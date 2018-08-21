38a2f64924c3140b0ba78c8904b2fd781f2fe23c
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-08-21 13:56:21 +0200

    add bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,20 +1,42 @@
 class BookingsController < ApplicationController
+  before_action :find_booking, only: [:show, :edit, :update, :destroy]
 
-  def new
-  end
-
-  def create
-  end
-
-  def update
+  def show
   end
 
   def edit
   end
 
-  def index
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
-  def show
+  def update
+    @booking.update(booking_params)
+    redirect_to booking_path
   end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def index
+    @bookings = Booking.all
+  end
+
+  private
+
+  def find_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:date, :content)
+  end
+
 end
