4aca6280115842100161e4836111ece18880a630
luca rampoldi <luca.rampoldi.it@gmail.com>
2018-08-24 14:37:46 +0200

    fixed picture bug

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,3 +1,7 @@
 class PhotoUploader < CarrierWave::Uploader::Base
-    include Cloudinary::CarrierWave
+  include Cloudinary::CarrierWave
+
+   def default_url
+    "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+   end
 end
